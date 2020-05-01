9c30b3eee9a23373cd361403ba5a995c99b418e0
mananmadan <madan.manan1@gmail.com>
2020-05-01 13:06:01 +0530

    auto update

---
+++ helper/backup.sh
@@ -0,0 +1,2 @@
+sudo cp index.noun index_backup.noun
+sudo cp data.noun  data_backup.noun 
---
+++ helper/run_mode.sh
@@ -0,0 +1,6 @@
+sudo rm index.noun
+sudo touch index.noun
+sudo rm data.noun
+sudo touch data.noun
+sudo cp index_backup.noun index.noun
+sudo cp data_backup.noun data.noun
